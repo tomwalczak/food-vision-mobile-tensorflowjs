583b1cd3fb68afd1f8ac1db6a13363222c760e31
tomwalchak <tom@tomwalczak.com>
2021-06-01 14:05:08 +0100

    Updates

---
+++ precache-manifest.53f6006f4489108331b7860dfcd75066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d66128ee2758910a8a1c8fd6a998108",
+    "url": "/food-vision-mobile-tensorflowjs/index.html"
+  },
+  {
+    "revision": "5645f50b27527fd3102a",
+    "url": "/food-vision-mobile-tensorflowjs/static/css/main.04a4f7a1.chunk.css"
+  },
+  {
+    "revision": "bda42727f842afeeaf2e",
+    "url": "/food-vision-mobile-tensorflowjs/static/js/2.5d228faa.chunk.js"
+  },
+  {
+    "revision": "e2abd2f3a71515dbc4d9a6c8477f7aa8",
+    "url": "/food-vision-mobile-tensorflowjs/static/js/2.5d228faa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5645f50b27527fd3102a",
+    "url": "/food-vision-mobile-tensorflowjs/static/js/main.3ce0e42f.chunk.js"
+  },
+  {
+    "revision": "415635189e5b38778bd6",
+    "url": "/food-vision-mobile-tensorflowjs/static/js/runtime-main.5a2887d2.js"
+  }
+]);
